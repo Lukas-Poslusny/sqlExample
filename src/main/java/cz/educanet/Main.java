bbd16d1c36fc56ef38d99898e8fe998904fc8c04
Lukas-Poslusny <48207694+Lukas-Poslusny@users.noreply.github.com>
2021-05-04 08:46:21 +0200

    first commit

--- src/main/java/cz/educanet/Main.java
+++ src/main/java/cz/educanet/Main.java
@@ -5,21 +5,22 @@ import java.sql.*;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        Connection connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost/c3zoo", "root", ""
-        );
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/c3zoo", "root", "");
 
         Statement statement = connection.createStatement();
-        ResultSet result = statement.executeQuery("SELECT Z.jmeno, D.nazev \n" +
-                "FROM Zvirata as Z join Druhy as D ON (Z.druh = D.id)\n" +
+        ResultSet result = statement.executeQuery("SELECT Z.id, Z.jmeno, D.nazev \n" +
+                "FROM Zvirata AS Z JOIN Druhy AS D ON (Z.druh = D.id)\n" +
+                "GROUP BY Z.id\n" +
                 "LIMIT 15");
 
+        String zvireId;
         String zvireJmeno;
         String druhNazev;
         while(result.next()) {
+            zvireId = result.getString("id");
             zvireJmeno = result.getString("jmeno");
             druhNazev = result.getString("nazev");
-            System.out.println("Jmeno: " + zvireJmeno + ", Druh: " + druhNazev);
+            System.out.println("Id: " + zvireId + ", Jmeno: " + zvireJmeno + ", Druh: " + druhNazev);
         }
 
         connection.close();
